e520da2f7743dd2a81fc55dcb5fc155685408130
Ander Linares <ander.linares@outlook.com>
2025-04-06 16:13:11 -0500

    new changes

---
+++ bin/up-prod.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+COMPOSE_PROVIDER="${COMPOSE_PROVIDER:-docker compose}"
+
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+PROJECT_ROOT="$(realpath "$SCRIPT_DIR/..")"
+
+cd ${PROJECT_ROOT}
+
+${COMPOSE_PROVIDER} \
+    -f ${PROJECT_ROOT}/docker/docker-compose.base.yml \
+    -f ${PROJECT_ROOT}/docker/docker-compose.prod.yml \
+    -f ${PROJECT_ROOT}/docker/docker-compose.traefik.yml \
+    up --detach
+
